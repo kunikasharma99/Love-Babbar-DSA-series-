9c128e23c4708558412eff0cfd106f731db38bd5
kunika sharma <63060253+kunikasharma99@users.noreply.github.com>
2022-01-11 02:57:04 +0530

    Completed ALL Patterns....DABANGG PATTERN ALSO !!

--- Patterns with spaces.cpp
+++ Patterns with spaces.cpp
@@ -131,7 +131,7 @@ while(i<=n){
           3 4
             4 */
 
-int n ;
+/*int n ;
 cout<<"Enter the value of n: \t";
 cin>>n;
 int i = 1;
@@ -148,5 +148,100 @@ while(i<=n){
   }
   cout<<endl;
   i++;
+}*/
+
+//PATTERN 7//
+/*         1
+         2 3
+       4 5 6
+     7 8 9 10    */
+
+/*int n;
+cout<<"Enter the value of n :\t";
+cin>>n;
+int i = 1;
+int count =1;
+while(i<=n){
+  int space = n-i;
+  while(space){
+    cout<<" ";
+    space--;
+  }
+  int j=1;
+  
+  while(j<=i){
+    
+    cout<<count++<<"";
+    j++;
+  }
+  cout<<endl;
+  i++;
+}*/
+
+//PATTERN 8 //
+/*       1
+       1 2 1
+     1 2 3 2 1
+   1 2 3 4 3 2 1    */
+
+ /*int n;
+ cout<<"Enter the value of n :\t";
+ cin>>n;
+ int i = 1;
+ while(i<=n){
+   //print space
+   int space = n-i;
+   while(space){
+     cout<<" ";
+     space--;
+   }
+   //print 2nd component [ 1st Triangle]
+   int j=1;
+   while(j<=i){
+     cout<<j;
+     j++;
+   }
+   //print 3rd component [ 2nd Triangle]
+   int start = i-1;
+   while(start){
+     cout<<start;
+     start--;
+   }
+   cout<<endl;
+   i++;
+ } */
+
+//PATTTERN 9 [DABANGG PATTERN]//
+/*    1 2 3 4 5 5 4 3 2 1
+      1 2 3 4 * * 4 3 2 1
+      1 2 3 * * * * 3 2 1
+      1 2 * * * * * * 2 1
+      1 * * * * * * * * 1   */
+
+/*int n ;
+cout<<"Enter the value of n :\t";
+cin>>n;
+int i=1;
+while(i<=n){
+  int j =1;
+  //1st Triangle 
+  while(j<=n-i+1){
+    cout<<j;
+    j++;
+  }
+  //Print STARS
+while(j<=n+i-1){
+cout<<"*";
+j++;
 }
+//2nd TRIANGLE
+int start = n-i+1;
+while(start){
+  cout<<start;
+  start--;
+  j++;
+}
+  cout<<endl;
+  i++;
+}*/
 } 
