09399fd61184c8240898752e4594e955d799d08b
kunika sharma <63060253+kunikasharma99@users.noreply.github.com>
2022-01-15 02:25:44 +0530

    Completed the Lecture 5.

---
+++ LECTURE-5  Scope of Variables , Operator Precedence, LEETCODE Questions (2).cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main() {
+// 1st QUESTION 
+
+/*int a = 3;
+cout<<a<<endl;
+if(true){
+  int a =5;
+  cout<<a<<endl;
+}  
+cout<<a<<endl;*/
+
+//2nd QUESTION 
+
+/*int a = 3;
+cout<<a<<endl;
+if(true){
+  int b =5;
+  cout<<b<<endl;
+}
+//int b = 1;  with comment ERROR 
+cout<<b<<endl;*/
+
+//3rd QUESTION 
+
+/*int a = 3;
+cout<<a<<endl;
+if(true){
+  int b =5;
+  cout<<b<<endl;
+}
+int b = 1;  
+cout<<b<<endl;
+
+//int b = 2;  //ERROR REDECLARATION
+cout<<b<<endl;*/
+
+//4th QUESTION 
+
+/*int i = 1;
+for(int i = 3; i<8;i++){
+  cout<<"Hi"<<endl;
+}
+cout<<i<<endl;*/
+
+//5th QUESTION
+
+/*int i = 7;
+for(; i<8;i++){
+  cout<<"Hi"<<endl;
+}
+cout<<i<<endl;*/
+
+https://leetcode.com/problems/number-of-1-bits/
+https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+} 
